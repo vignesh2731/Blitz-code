3b65d239182733c9f54f606ab8bc5525474c729e
sreevignesh2731 <sreevignesh27@gmail.com>
2025-07-28 23:55:54 +0530

    Added a report issue page

---
+++ apps/web/app/report-issue/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useState } from "react"
+import InputBox from "../../components/InputBox";
+
+export default function Report()
+{
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+    return <div className="flex flex-col justify-center px-60 pt-24">
+        <div className="bg-slate-200 h-[500px]">
+            <div className="flex flex-col items-center pt-32 h-full gap-10">
+                <div className="text-4xl font-bold">
+                    Report an Issue 
+                </div>
+                <InputBox label="Issue title : " placeholder="" type="text" onChange={(value)=>{
+                    setTitle(value)
+                }}/>
+                <InputBox label="Description : " placeholder="" type="text" onChange={(value)=>{
+                    setDescription(value);
+                }}/>
+            </div>
+        </div>
+    </div>
+}
