b80e2c13df9a1c12639426ace4eb0e078abf9f4a
sreevignesh2731 <sreevignesh27@gmail.com>
2025-07-29 23:37:00 +0530

    Corrected onClick routes

--- apps/web/components/SideBar.tsx
+++ apps/web/components/SideBar.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import { SideBarItem } from "./SideBarItem";
 export function SideBar(){
-    let sideBarArray=[{name:"Create a Contest",link:"/createContest"},{name:"Join Contest",link:"/joinContest"},{name:"Check Result",link:"/checkResult"},{name:"Report an issue",link:"/issue"},]
+    let sideBarArray=[{name:"Create a Contest",link:"/createContest"},{name:"Join Contest",link:"/joinContest"},{name:"Check Result",link:"/checkResult"},{name:"Report an issue",link:"/report-issue"},]
     return  <motion.div
       className="h-screen bg-slate-200  flex flex-col justify-evenly px-10 shadow-md relative pb-20"
       initial={{ opacity: 0, x: -200 }}
